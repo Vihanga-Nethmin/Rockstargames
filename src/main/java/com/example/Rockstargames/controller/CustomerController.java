83b7b0e5e58d9ec2d6fcfc35a4f6213277699c73
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-12 10:38:42 +0530

    create index.java

--- src/main/java/com/example/Rockstargames/controller/CustomerController.java
+++ src/main/java/com/example/Rockstargames/controller/CustomerController.java
@@ -1,4 +1,8 @@
 package com.example.Rockstargames.controller;
 
 public class CustomerController {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
 }
