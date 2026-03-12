89195096c07b3fcf950674b4f027f2c2554a094f
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-12 10:27:34 +0530

    Star to create project

---
+++ src/main/java/com/example/Rockstargames/RockstargamesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Rockstargames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockstargamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockstargamesApplication.class, args);
+	}
+
+}
