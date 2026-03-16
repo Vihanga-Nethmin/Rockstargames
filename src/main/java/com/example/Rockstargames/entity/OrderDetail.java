3ed66e7ea7b25987c52e7d0292a071e0c02aae68
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-16 14:45:52 +0530

    updated OrderDetail entity by adding entities

--- src/main/java/com/example/Rockstargames/entity/OrderDetail.java
+++ src/main/java/com/example/Rockstargames/entity/OrderDetail.java
@@ -1,4 +1,19 @@
 package com.example.Rockstargames.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class OrderDetail {
+    @Id
+    private String orderId;
+    private String order_date;
+    private String total_amount;
+    private String customer_id;
 }
