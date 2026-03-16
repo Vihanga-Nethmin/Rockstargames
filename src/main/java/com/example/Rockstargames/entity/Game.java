6b8228501ba28ae2f79eb6dedb9ba3f1b2955701
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-16 14:42:57 +0530

    updated game entity by adding entities

--- src/main/java/com/example/Rockstargames/entity/Game.java
+++ src/main/java/com/example/Rockstargames/entity/Game.java
@@ -1,4 +1,19 @@
 package com.example.Rockstargames.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Game {
+    @Id
+    private String gameId;
+    private String title;
+    private String description;
+    private String price;
 }
