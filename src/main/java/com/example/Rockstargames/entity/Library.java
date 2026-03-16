83957dbe30750d897eca49f87fde35bd543a97a9
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-16 15:10:19 +0530

    updated Library entity by adding entities

--- src/main/java/com/example/Rockstargames/entity/Library.java
+++ src/main/java/com/example/Rockstargames/entity/Library.java
@@ -1,4 +1,20 @@
 package com.example.Rockstargames.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
 public class Library {
+    @Id
+    private String library_id;
+    private String customer_id;
+    private String game_id;
+    private String purchase_date;
 }
