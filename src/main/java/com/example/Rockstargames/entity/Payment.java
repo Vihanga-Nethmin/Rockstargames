8dccafe5799dd1419e9f5cf29dec3c802ba295e1
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-16 15:05:12 +0530

    updated Payment.java entity by adding entities

--- src/main/java/com/example/Rockstargames/entity/Payment.java
+++ src/main/java/com/example/Rockstargames/entity/Payment.java
@@ -1,4 +1,22 @@
 package com.example.Rockstargames.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Payment {
+    @Id
+    private String payment_id;
+    private String order_id;
+    private String payment_method;
+    private String payment_status;
+    private String payment_date;
+    private String transaction_id;
+
 }
