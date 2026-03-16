0e4cbcf54383a11e35b3007f0dc77a618e0520af
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-16 14:40:51 +0530

    updated customer entity by adding entities

--- src/main/java/com/example/Rockstargames/entity/Customer.java
+++ src/main/java/com/example/Rockstargames/entity/Customer.java
@@ -1,4 +1,21 @@
 package com.example.Rockstargames.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Customer {
+@Id
+    private String customer_id;
+    private String name;
+    private String email;
+    private String  password;
+
+
 }
