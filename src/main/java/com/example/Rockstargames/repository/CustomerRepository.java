d012bc29ab1ea97270081ed4b9dc67394f7c4c37
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-18 15:42:51 +0530

    Add CustomerRepository interface

--- src/main/java/com/example/Rockstargames/repository/CustomerRepository.java
+++ src/main/java/com/example/Rockstargames/repository/CustomerRepository.java
@@ -1,4 +1,10 @@
 package com.example.Rockstargames.repository;
 
-public interface CustomerRepository {
+import com.example.Rockstargames.entity.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer,String> {
 }
