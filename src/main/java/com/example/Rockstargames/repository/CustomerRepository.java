2d9694b4b9a8d20c898aeb24cf1cebac559fefda
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-20 15:55:51 +0530

    change CrudRepo to the JpaRepo

--- src/main/java/com/example/Rockstargames/repository/CustomerRepository.java
+++ src/main/java/com/example/Rockstargames/repository/CustomerRepository.java
@@ -1,10 +1,11 @@
 package com.example.Rockstargames.repository;
 
 import com.example.Rockstargames.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer,String> {
+public interface CustomerRepository extends JpaRepository<Customer,String> {
 }
