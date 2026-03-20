993a82ef67e6fd030a63be1886ccdb38ee76e20d
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-20 20:57:47 +0530

    add validation

--- src/main/java/com/example/Rockstargames/dto/OrderDto.java
+++ src/main/java/com/example/Rockstargames/dto/OrderDto.java
@@ -1,10 +1,23 @@
 package com.example.Rockstargames.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDto {
+    @NotNull(message = "Order Id is mandatory")
+    private String order_id;
+
+    @NotNull(message = "Game Id is mandatory")
+    private String game_id;
+
+    @NotBlank(message = "price is mandatory")
+    private String price;
 
 
 }
