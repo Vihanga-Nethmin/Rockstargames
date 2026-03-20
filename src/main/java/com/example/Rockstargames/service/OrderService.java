d783e1173a8d12407157cd5615052a982ac1dc02
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-20 20:03:47 +0530

    do implementation according to methods

--- src/main/java/com/example/Rockstargames/service/OrderService.java
+++ src/main/java/com/example/Rockstargames/service/OrderService.java
@@ -1,4 +1,16 @@
 package com.example.Rockstargames.service;
 
+import com.example.Rockstargames.dto.OrderDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface OrderService {
+    void save(OrderDto orderdto);
+    void update(OrderDto orderdto);
+    void delete(String id);
+    List<OrderDto> getAll();
+
 }
--- src/main/java/com/example/Rockstargames/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/Rockstargames/service/impl/OrderServiceImpl.java
@@ -1,4 +1,28 @@
 package com.example.Rockstargames.service.impl;
 
-public class OrderServiceImpl {
+import com.example.Rockstargames.dto.OrderDto;
+import com.example.Rockstargames.service.OrderService;
+
+import java.util.List;
+
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public void save(OrderDto orderdto) {
+
+    }
+
+    @Override
+    public void update(OrderDto orderdto) {
+
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+
+    @Override
+    public List<OrderDto> getAll() {
+        return List.of();
+    }
 }
