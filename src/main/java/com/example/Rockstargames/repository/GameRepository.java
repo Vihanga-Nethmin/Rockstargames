c16478fa9aa3f0d6d627fa40429ee1dac75e3baa
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-20 15:57:11 +0530

    add GameRepository

--- src/main/java/com/example/Rockstargames/repository/GameRepository.java
+++ src/main/java/com/example/Rockstargames/repository/GameRepository.java
@@ -1,4 +1,8 @@
 package com.example.Rockstargames.repository;
 
-public interface GameRepository {
+import com.example.Rockstargames.entity.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GameRepository extends JpaRepository<Game,String> {
 }
