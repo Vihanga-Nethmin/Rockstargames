41a72981f51deef56bc1d6682639766071445567
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-22 11:23:59 +0530

    Do save Function

--- src/main/java/com/example/Rockstargames/controller/PaymentController.java
+++ src/main/java/com/example/Rockstargames/controller/PaymentController.java
@@ -1,4 +1,42 @@
 package com.example.Rockstargames.controller;
 
+
+import com.example.Rockstargames.dto.CustomerDto;
+import com.example.Rockstargames.dto.PaymentDto;
+import com.example.Rockstargames.service.CustomerService;
+import com.example.Rockstargames.service.PaymentService;
+import com.example.Rockstargames.utill.APIResponse;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(originPatterns = "*")
+@RestController
+@RequestMapping("api/v1/payment")
+
 public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<APIResponse<String>> addPayment(@RequestBody @Valid PaymentDto paymentDto) {
+        paymentService.save(paymentDto);
+        return new ResponseEntity<>(new APIResponse<>(201, "Payment Saved", null), HttpStatus.CREATED);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/example/Rockstargames/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/example/Rockstargames/service/impl/PaymentServiceImpl.java
@@ -1,13 +1,41 @@
 package com.example.Rockstargames.service.impl;
 
 import com.example.Rockstargames.dto.PaymentDto;
+import com.example.Rockstargames.entity.Customer;
+import com.example.Rockstargames.entity.Payment;
+import com.example.Rockstargames.exception.CustomException;
+import com.example.Rockstargames.repository.CustomerRepository;
+import com.example.Rockstargames.repository.PaymentRepository;
 import com.example.Rockstargames.service.PaymentService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PaymentServiceImpl implements PaymentService {
+
+
+
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private PaymentRepository paymentRepository;
+
     @Override
     public void save(PaymentDto paymentdto) {
+        if (paymentdto==null){
+            throw new NullPointerException("PaymentDto is null");
+        }
+        if (paymentRepository.existsById(paymentdto.getPayment_id())){
+
+            throw new CustomException("Customer already exists");
+
+
+        }
+        paymentRepository.save(modelMapper.map(paymentdto, Payment.class));
+
 
     }
 
