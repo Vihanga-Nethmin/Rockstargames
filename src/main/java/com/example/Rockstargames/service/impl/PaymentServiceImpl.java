b88eee5432ddc434468dde0691ddfc93047c5ae0
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-22 11:01:29 +0530

    add PaymentServiceImpl skeleton with unimplemented methods

--- src/main/java/com/example/Rockstargames/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/example/Rockstargames/service/impl/PaymentServiceImpl.java
@@ -1,4 +1,28 @@
 package com.example.Rockstargames.service.impl;
 
-public class PaymentServiceImpl {
+import com.example.Rockstargames.dto.PaymentDto;
+import com.example.Rockstargames.service.PaymentService;
+
+import java.util.List;
+
+public class PaymentServiceImpl implements PaymentService {
+    @Override
+    public void save(PaymentDto paymentdto) {
+
+    }
+
+    @Override
+    public void update(PaymentDto paymentdto) {
+
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+
+    @Override
+    public List<PaymentDto> getAll() {
+        return List.of();
+    }
 }
