56c31591590307f8ba1ce8e8c8f2c085f9be7248
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-22 10:59:18 +0530

    feat: add PaymentService interface for payment management

--- src/main/java/com/example/Rockstargames/service/PaymentService.java
+++ src/main/java/com/example/Rockstargames/service/PaymentService.java
@@ -1,4 +1,12 @@
 package com.example.Rockstargames.service;
 
+import com.example.Rockstargames.dto.PaymentDto;
+
+import java.util.List;
+
 public interface PaymentService {
+    void save(PaymentDto paymentdto);
+    void update(PaymentDto paymentdto);
+    void delete(String id);
+    List<PaymentDto> getAll();
 }
