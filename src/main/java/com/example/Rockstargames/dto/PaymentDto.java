06bfdf6163ca78a5da2981b6cdbe8d49256af4f7
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-22 11:14:20 +0530

    create PaymentDto with validation annotations

--- src/main/java/com/example/Rockstargames/dto/PaymentDto.java
+++ src/main/java/com/example/Rockstargames/dto/PaymentDto.java
@@ -1,4 +1,31 @@
 package com.example.Rockstargames.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentDto {
+
+    @NotNull(message = "Payment Id is mandatory")
+    private String payment_id;
+
+    @NotNull(message = "Order Id is mandatory")
+    private String order_id;
+
+    @NotBlank(message = "Payment date is mandatory")
+    private String payment_date;
+
+    @NotBlank(message = "Payment method is mandatory")
+    private String payment_method;
+
+    @NotBlank(message = "Payment status is mandatory")
+    private String payment_status;
+
+    @NotBlank(message = "Transaction Id is mandatory")
+    private String transaction_id;
 }
