543b6779456c276b2a674285ea870033ab9b64b0
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-03-23 21:33:57 +0530

    add some changes in html pages

--- src/main/java/com/example/Rockstargames/entity/OrderDetail.java
+++ src/main/java/com/example/Rockstargames/entity/OrderDetail.java
@@ -2,6 +2,7 @@ package com.example.Rockstargames.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +11,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@IdClass(OrderDetailPK.class) // Composite Key එක මෙතනට ඕනේ
 public class OrderDetail {
     @Id
     private String order_id;
+    @Id
+    private String customer_id; // PK එකට අදාළ field එක
+
     private String order_date;
     private String total_amount;
-    private String customer_id;
 }
